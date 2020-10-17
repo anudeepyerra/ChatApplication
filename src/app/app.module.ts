1147f03ef83ca78cafafb76b13b3fcb417e43dae
Anudeep <anudeepyerra1805@gmail.com>
2020-10-17 16:12:49 +0530

    firstCommit

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { UserModule } from './user/user.module';
+import { ChatModule } from './chat/chat.module';
+import { RouterModule } from '@angular/router';
+import { LoginComponent } from './user/login/login.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ChatModule,
+    UserModule,
+    RouterModule.forRoot([
+      {path : 'login', component : LoginComponent, pathMatch: 'full' },
+      {path : '', redirectTo : 'login', pathMatch: 'full' },
+      {path : '*', component : LoginComponent},
+      {path : '**', component : LoginComponent}
+    ])
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
